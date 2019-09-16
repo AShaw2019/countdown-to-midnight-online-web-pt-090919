29bbf8b9136ba63f740d04c2807420753f4d6d85
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 03:43:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,9 @@
-def countdown(time_to_midnight)
+def countdown_with_sleep(time_to_midnight)
   while time_to_midnight > 0
     puts "#{time_to_midnight} SECOND(S)!"
+    sleep(1)
     time_to_midnight -= 1
   end
-  time_to_midnight.to_s + " left. HAPPY NEW YEAR!"
   "HAPPY NEW YEAR!"
 end
-  
-  def countdown_with_sleep(time_to_midnight)
-  sleep(1)
-    time_to_midnight -= 1
-  end
-  
 
