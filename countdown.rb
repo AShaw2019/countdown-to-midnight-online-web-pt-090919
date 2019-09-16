155a121f8b5a6d0c57a0d4164ed977e23d26cb90
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 03:39:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
-def countdown(10)
-  
-  new_year = 10
-  while new_year < 10
-  puts string
- new_year += 1
- end
+def countdown(time_to_midnight)
+  while time_to_midnight > 0
+    puts "#{time_to_midnight} SECOND(S)!"
+    time_to_midnight -= 1
+  end
+  time_to_midnight.to_s + " left. HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
   
   
